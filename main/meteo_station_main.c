783537f0eed13f224428e16b4b378db46d584a3b
Sergey Yasinsky <seregajsv@gmail.com>
2020-12-31 15:22:43 +0300

    Fixed license header.

--- main/meteo_station_main.c
+++ main/meteo_station_main.c
@@ -1,6 +1,4 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
+/* This application code is in the Public Domain (or CC0 licensed, at your option.)
 
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
